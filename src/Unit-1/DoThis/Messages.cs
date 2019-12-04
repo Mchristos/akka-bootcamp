d4646a725e5696ac9e29fbd78c2f0aab9daad117
chris <chrismarais@protonmail.com>
2019-12-04 12:57:30 +0200

    lesson 2

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace WinTail
+{
+    public class Messages
+    {
+        #region Neutral/system messages
+        /// <summary>
+        /// Marker class to continue processing.
+        /// </summary>
+        public class ContinueProcessing
+        {
+
+        }
+        #endregion
+
+        #region Success messages
+        /// <summary>
+        /// Base class to signal valid user input. 
+        /// </summary>
+        public class InputSuccess
+        {
+            public string Reason { get; private set; }
+            public InputSuccess(string reason)
+            {
+                Reason = reason;
+            }
+        }
+        #endregion
+
+        #region Error messages
+        /// <summary>
+        /// Base class to signal invalid user input. 
+        /// </summary>
+        public class InputError
+        {
+            public string Reason { get; private set; }
+            public InputError(string reason)
+            {
+                Reason = reason;
+            }
+        }
+
+        /// <summary>
+        /// User provided blank input.
+        /// </summary>
+        public class NullInputError : InputError
+        {
+            public NullInputError(string reason) : base(reason) { }
+        }
+
+        /// <summary>
+        /// User provided invalid input (currently, input w/ odd # chars)
+        /// </summary>
+        public class ValidationError : InputError
+        {
+            public ValidationError(string reason) : base(reason) { }
+        }
+        #endregion
+    }
+}
--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -10,7 +10,6 @@ namespace WinTail
 
         static void Main(string[] args)
         {
-            PrintInstructions();
 
             // Initialize actor system
             MyActorSystem = ActorSystem.Create("MyActorSystem");
@@ -20,27 +19,13 @@ namespace WinTail
             IActorRef consoleReaderActor = MyActorSystem.ActorOf(Props.Create( () => new ConsoleReaderActor(consoleWriterActor)));
 
             // tell console reader to begin
-            consoleReaderActor.Tell("bla bla");
+            consoleReaderActor.Tell(ConsoleReaderActor.StartCommand);
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.WhenTerminated.Wait();
         }
 
-        private static void PrintInstructions()
-        {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.Write("Some lines will appear as");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write(" red ");
-            Console.ResetColor();
-            Console.Write(" and others will appear as");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(" green! ");
-            Console.ResetColor();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
-        }
+
     }
     #endregion
 }
